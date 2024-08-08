6dfa3129e70a98909837a979d872ebc9898bcf77
a amateur! <141594016+CheetosDeLimon@users.noreply.github.com>
2024-08-08 08:23:55 -0500

    SEGUNDO COMMIT (script suma en app.js)

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hola mundo :v')
+//SUMA DE 2 NUMEROS TOTALMENTE FUNCIONAL :D
+
+let num1 = parseInt(prompt('Ingrese el primer numero: '));
+let num2 = parseInt(prompt('Ingrese el segundo numero: '));
+
+let resultado = num1 + num2;
+
+console.log('La Suma es: ', resultado)
