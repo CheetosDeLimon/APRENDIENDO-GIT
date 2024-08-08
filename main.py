792a6ada8832d8b3096e6fc476172f6291c9edb9
a amateur! <141594016+CheetosDeLimon@users.noreply.github.com>
2024-08-08 09:00:26 -0500

    CUARTO COMMIT (Nuevo archivo .py con suma)

---
+++ main.py
@@ -0,0 +1,10 @@
+print ('HOLA NEGROS')
+
+#SOLO SUMA 2 NUMEROS 
+num1 = int(input('Ingrese el primer numero: '))
+
+num2 = int(input('Ingrese el segundo numero: '))
+
+resultado = num1 + num2
+
+print ('El resultado de su suma es: ', resultado)
